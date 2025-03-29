7306d37b95b5a5f1bbfdb6c057723cbe6c48d20c
mrinmoy-765 <mrinmoyd629@gmail.com>
2025-03-29 11:34:29 +0600

    database config fixed

--- coffe-store-server/index.js
+++ coffe-store-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require ('dotenv').config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
@@ -9,7 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://ExpressoEmporium:expressoEmporium@cluster0.ncoxxcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncoxxcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
